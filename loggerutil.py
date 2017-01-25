2ab4c88f49a6fba6fa39539bdfed01b5ef96e3e1
fiht <me@fiht.me>
2017-01-25 20:18:36 +0800

    NetmaskScanner_demo.py added

---
+++ loggerutil.py
@@ -0,0 +1,35 @@
+import logging
+
+
+class logger_util():
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def get_formatter():
+        """get_logger"""
+        logger = logging.getLogger(name='UtilLogger')
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        return logger
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def log_warning(msg):
+        """log warning msg"""
+        logger_util.get_formatter().warning(msg)
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def log_debug(msg):
+        """log debug msg"""
+        logger_util.get_formatter().debug(msg)
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def log_error(msg):
+        """log error msg"""
+        logger_util.get_formatter().error(msg)
+
